b5e1ba46450107a2b28bb0758cc42813e3fc591b
Ishwarya-arumugam <ishwaryaa44@gmail.com>
2016-07-08 12:13:40 +0530

    Create robot.c

---
+++ robot.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,x,y;
+clrscr();
+if(x>0&&x<=10000)
+{
+for(i=0;i<=x;i++)
+{
+printf("Command 1:%d",x);
+}
+}
+if(y==-180&&y==180)
+{
+for(i=0;i<=y;i++)
+{
+printf("Command 2:%d",y);
+}
+}
+getch();
+}
